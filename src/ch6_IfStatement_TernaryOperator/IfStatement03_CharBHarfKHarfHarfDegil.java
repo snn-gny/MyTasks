0364c599bdc5e1a4d0c6acf0368a1c6ee5003955
Yildiz <yldzgny34@gmail.com>
2023-05-16 19:48:39 +0200

    dienstag

--- src/ch6_IfStatement_TernaryOperator/IfStatement03_CharBHarfKHarfHarfDegil.java
+++ src/ch6_IfStatement_TernaryOperator/IfStatement03_CharBHarfKHarfHarfDegil.java
@@ -1,5 +1,7 @@
 package ch6_IfStatement_TernaryOperator;
 
+import java.util.Scanner;
+
 public class IfStatement03_CharBHarfKHarfHarfDegil {
 
 	public static void main(String[] args) {
@@ -9,11 +11,16 @@ public class IfStatement03_CharBHarfKHarfHarfDegil {
 		 Bu character kucuk harf ise "Kucuk Harf" yazdirin
 		 Bu character harf degil ise "Harf Degil" yazdirin
 		*/
-		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Bir karakter girin");
+		char c=sc.next().charAt(0);
+		if (Character.isUpperCase(c)) {
+			System.out.println(c+" Büyük harftir");}
+		else if (Character.isLowerCase(c)) {
+			System.out.println(c+" Kücük harftir");
+		}else System.out.println("Harf Degil");
 
-		
 
-
-} 
+	}
 
 }
